158f6968b31807c938d089af271045438951f29b
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-18 18:10:40 -0600

    Add files via upload

---
+++ iHoot Articles/articleThree.js
@@ -0,0 +1,6 @@
+let Clue = document.getElementById('clue')
+let btnCalcClue = document.getElementById('btn')
+
+btnCalcClue.onclick = function () {
+    Clue.value = "____'s _____ ____ is where you can read more about colleges and many other school topics."
+}
