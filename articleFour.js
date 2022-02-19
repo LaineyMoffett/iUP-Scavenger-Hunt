3c71f795ff7a9a6fed570638a9739c310ecd8e87
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-18 18:11:35 -0600

    Delete articleFour.js

--- articleFour.js
+++
@@ -1,6 +0,0 @@
-let Clue = document.getElementById('clue')
-let btnCalcClue = document.getElementById('btn')
-
-btnCalcClue.onclick = function () {
-    Clue.value = "This is a well-known university, but the original  ___ University Hall burned down in ___."
-}
