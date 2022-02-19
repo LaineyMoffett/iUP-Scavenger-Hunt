b6a6db2f07472385878f6ea2d53c9d67d6c07e24
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-18 18:11:43 -0600

    Delete articleOne.js

--- articleOne.js
+++
@@ -1,6 +0,0 @@
-let Clue = document.getElementById('clue')
-let btnCalcClue = document.getElementById('btn')
-
-btnCalcClue.onclick = function () {
-    Clue.value = "___ University has many options for majors and minors and features the ____, where anyone can visit and see all kinds of plants."
-}
