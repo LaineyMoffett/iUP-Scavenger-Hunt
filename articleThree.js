ee50bfc1df370f5fac594216093c16f028f7a385
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-18 18:11:50 -0600

    Delete articleThree.js

--- articleThree.js
+++
@@ -1,6 +0,0 @@
-let Clue = document.getElementById('clue')
-let btnCalcClue = document.getElementById('btn')
-
-btnCalcClue.onclick = function () {
-    Clue.value = "____'s _____ ____ is where you can read more about colleges and many other school topics."
-}
