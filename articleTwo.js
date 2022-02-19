edcf5feaf9c5094b0a5f5f80d4a5ea729a747efd
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-18 18:11:56 -0600

    Delete articleTwo.js

--- articleTwo.js
+++
@@ -1,6 +0,0 @@
-let Clue = document.getElementById('clue1')
-let btnCalcClue = document.getElementById('btn')
-
-btnCalcClue.onclick = function () {
-    Clue.value = "This lesser-known college was one of the first all-girls schools."
-}
