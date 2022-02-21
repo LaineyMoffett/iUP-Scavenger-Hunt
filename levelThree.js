497d331d7379e0d59ee83f4a58d715508ada42b2
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-20 20:27:06 -0600

    Update levelThree.js

--- levelThree.js
+++ levelThree.js
@@ -6,37 +6,37 @@ btnCalcNxtClue.onclick = function (){
     let now_or_later = prompt ("Are you ready for the next key? Type '1' to continue.")
     if(now_or_later == 1) {
 
-        let nearest_or_futher = prompt ("In high school, this teacher worked at both ____ and ____. Type 2 to move onto the next clue.")
+        let nearest_or_futher = prompt ("In high school, this teacher worked at both ____ and ____. Type 2 to move on to the next clue.")
         if(nearest_or_futher == 2){
 
-          let yes_or_no =  prompt( "This teacher, who may be called by different names by students, graduated from _____. Type 3 to move onto the next clue.")
+          let yes_or_no =  prompt( "This teacher, who may be called by different names by students, graduated from _____. Type 3 to move on to the next clue.")
           if(yes_or_no == 3) {
 
 
 
-            let no_no = prompt ("A teacher who has many talents such as knowing martial arts is ____.  Type 4 to move onto the next clue")
+            let no_no = prompt ("A teacher who has many talents such as knowing martial arts is ____.  Type 4 to move on to the next clue")
             if(no_no == 4) {
 
-            let yess_no = prompt ("If you’re taking ASL, you’re guaranteed to have ____ who is also a leader of this club. Type 5 to move onto the next clue.")
+            let yess_no = prompt ("If you’re taking ASL, you’re guaranteed to have ____ who is also a leader of this club. Type 5 to move on to the next clue.")
             if(yess_no == 5) {
             
-            let yesss_no = prompt ("_____ teachers the only AP foreign language and went to __ colleges. Type 6 to move onto the next clue.")
+            let yesss_no = prompt ("_____ teachers the only AP foreign language and went to __ colleges. Type 6 to move on to the next clue.")
             if(yesss_no == 6){
-                let yes_or_yes = prompt ("This teacher is trilingual and went to the prestigious ____. Type 7 to move onto the next clue.")
+                let yes_or_yes = prompt ("This teacher is trilingual and went to the prestigious ____. Type 7 to move on to the next clue.")
                 if(yes_or_yes == 7) {
         
-                let next_clue = prompt ("____ teaches history courses and graduated with a ____ ____ degree; she also is a sponsor of this club. Type 8 to move onto the next clue.")
+                let next_clue = prompt ("____ teaches history courses and graduated with a ____ ____ degree; she also is a sponsor of this club. Type 8 to move on to the next clue.")
                 if(next_clue == 8) {
-                    let nxt_clue1 = prompt ("If you want to join a club at our school, you can visit the ___ page on the ___ ____. Type 9 to move onto the next clue.")
+                    let nxt_clue1 = prompt ("If you want to join a club at our school, you can visit the ___ page on the ___ ____. Type 9 to move on to the next clue.")
                     if(nxt_clue1 == 9) {
                     let next_clue_2 = prompt ("If you’re considering pursuing a career in journalism or media, you can join iUP’s ____. Type 10 to move on.")
                     if(next_clue_2 == 10) {
-                        let next_clue_3 = prompt ("This club has many leadership positions for students, and may even be putting on College and Career Week. Type 11 to move onto the next key.")
+                        let next_clue_3 = prompt ("This club has many leadership positions for students, and may even be putting on College and Career Week. Type 11 to move on to the next key.")
 
                     if (next_clue_3 == 11) {
-                        let next_clue_4 = prompt ("If you join ___ ___, you can learn the ins and outs of a game that's over 2000 years old!. Type 12 to move onto the next key.")
+                        let next_clue_4 = prompt ("If you join ___ ___, you can learn the ins and outs of a game that's over 2000 years old!. Type 12 to move on to the next key.")
                         if(next_clue_4 == 12) {
-                            let next_clue_5 = prompt ("It’s no doubt that a Culinary school would love to see this club, hosted by ___, ___, ____, ____, and ___, on your resume. Type 13 to move onto the next clue.")
+                            let next_clue_5 = prompt ("It’s no doubt that a Culinary school would love to see this club, hosted by ___, ___, ____, ____, and ___, on your resume. Type 13 to move on to the next clue.")
                             if(next_clue_5 == 13) {
                                 let next_cluee_6 = prompt ("Clubs are great things to add to your ____ and they appeal to many ___.  Type 14 to move on.")
                                 if(next_cluee_6 == 14) {
