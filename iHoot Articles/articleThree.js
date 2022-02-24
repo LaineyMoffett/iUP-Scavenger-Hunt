2bc131e6366d04ccd56ff07a983e87ac03192faa
Lainey Moffett <94018322+LaineyMoffett@users.noreply.github.com>
2022-02-23 23:30:05 -0600

    Update articleThree.js

--- iHoot Articles/articleThree.js
+++ iHoot Articles/articleThree.js
@@ -2,5 +2,5 @@ let Clue = document.getElementById('clue')
 let btnCalcClue = document.getElementById('btn')
 
 btnCalcClue.onclick = function () {
-    Clue.value = "____'s _____ ____ is where you can read more about colleges and many other school topics."
+    Clue.value = "____'s _____ ____ is where you can read student's conent about colleges and many other school topics."
 }
